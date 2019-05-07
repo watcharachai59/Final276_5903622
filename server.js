c3961202f9bcb1f7061a847ab040ecf4120fb81e
watcharachai59 <46855973+watcharachai59@users.noreply.github.com>
2019-05-07 16:24:28 +0700

    Update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var port = process.env.PORT || 3000;
 var io = require('socket.io')(port);
-console.log("server started"+port);
+console.log("server started");
 var Random = 0;
 io.on("connection",function(socket){
     console.log("Client connected");
